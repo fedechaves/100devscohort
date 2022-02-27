3cd2019a58f23f23cf34921c33f6a2686c0eba8f
ksh0t <90200540+ksh0t@users.noreply.github.com>
2022-02-27 18:33:40 -0300

    Add files via upload

---
+++ class12-materials/background-picker/js/main.js
@@ -0,0 +1,29 @@
+document.getElementById('purple').onclick = partyPurple
+document.getElementById('green').onclick = partyGreen
+document.getElementById('blue').onclick = partyBlue
+document.getElementById('black').onclick = partyBlack
+document.getElementById('skyblue').onclick = partySkyBlue
+
+function partyPurple(){
+  document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function partyGreen() {
+  document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function partyBlue() {
+  document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
+  document.querySelector('body').style.color = 'white'
+}
+
+function partyBlack() {
+  document.querySelector('body').style.backgroundColor= 'rgba(0,0,0)'
+  document.querySelector('body').style.color = 'white'
+}
+function partySkyBlue() {
+  document.querySelector('body').style.backgroundColor= 'rgba(135, 206, 235)'
+  document.querySelector('body').style.color= 'white'
+}
