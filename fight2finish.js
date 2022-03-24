501bbcfdfe33bd58eb69c91c4ed65b64a9004add
fedechaves <90200540+fedechaves@users.noreply.github.com>
2022-03-24 11:56:03 -0300

    Add files via upload

---
+++ fight2finish.js
@@ -0,0 +1,83 @@
+//Create an array of movie titles. 
+//Loop through the array and add each element to 
+//the h2.
+//let movies = ["rocky", "coach carter", "interstellar"]
+
+//for (i=0; i<movies.length; i++){
+ //   document.querySelector('h2').innerText += movies[i]
+
+//}//
+//Create an array of numbers. 
+//Loop through the array and three to each 
+//number and replace the old number.
+//let numbers = [1,2,3,4,5]
+
+//numbers.forEach((item,i) => {
+ //   numbers.forEach((item,i) => {
+  //  numbers[i] = item + 3
+//})
+
+//Find the average of all the numbers from question three
+//let sum = 0
+//for (let i=0; i<numbers.length; i++){
+ //   sum += numbers[i]
+////}
+//console.log(sum/numbers.length)
+
+//let array1 = [true,  true,  true,  false,
+    // true,  true,  true,  true ,
+    // true,  false, true,  false,
+    // true,  false, false, true ,
+    // true,  true,  true,  true ,
+    // false, false, true,  true ]
+
+// function countSheeps(arrayOfSheep) {
+//     let ts = 0
+//     arrayOfSheep.forEach(item => {
+//         if (item === true) {
+//             ts +=1
+//         }
+//     })
+//     return ts
+
+// }
+// console.log(countSheeps(array1))
+
+
+// function countSheeps(arrayOfSheep) {
+//     let ts = 0
+//     arrayOfSheep.forEach(item => {
+//         if (item) counter++
+//     })
+//     return ts
+
+// }
+// console.log(countSheeps(array1))
+
+// function countSheeps(arrayOfSheep) {
+//     let ts = 0
+//     for(let i = 0; i < arrayOfSheep.length; i++) {
+//         if(arrayOfSheep[i]){ 
+//             ts += 1
+//         }
+//     }
+//     return ts
+// }
+// console.log(countSheeps(array1))
+// //codewars
+// function positiveSum(arr) {
+//     var sum = 0;
+//     for(var i = 0; i < arr.length; i++) {
+//       if(arr[i] > 0) {
+//        sum += arr[i];
+//      }
+//     }
+//    return sum;
+//    }
+
+//otra funcion
+alert('hola')
+const palabras = ['manzana', 'pera','pinha']
+
+
+const smash = words => words.join(' ')
