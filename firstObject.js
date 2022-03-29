d7ceb73bb40fd1667aa32dacaad30b3bc31fac2a
fedechaves <90200540+fedechaves@users.noreply.github.com>
2022-03-29 15:53:12 -0400

    Add files via upload

---
+++ firstObject.js
@@ -0,0 +1,34 @@
+//Create a stopwatch object that has four properties and three methods
+
+// let stopwatch = {}
+
+// stopwatch.brand = 'Nike'
+// stopwatch.color = 'black'
+// stopwatch.shape = 'round'
+// stopwatch.size = 'shmmmedium'
+
+// stopwatch.start = function(){
+//     console.log('STARTTTINNNNGGGG')
+// }
+// stopwatch.stop = function(){
+//     console.log('STOPPPINNGGG')
+// }
+// stopwatch.sayBrand = function(){
+//     console.log( stopwatch.brand  )
+// }
+
+let stopwatch = {}
+stopwatch.color = 'black'
+stopwatch.currentTime = 2
+stopwatch.shape = 'round'
+stopwatch.size = 'large'
+
+stopwatch.start = function(){
+    console.log('starting')
+}
+stopwatch.end = function(){
+    console.log('end')
+}
+stopwatch.saybrand = function(){
+    console.log('adidas')
+}
