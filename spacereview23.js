51b9ae3ac780101ef270a27ce49b683e175fc8e0
fedechaves <federicochavesf@gmail.com>
2022-04-05 15:02:00 -0400

    alzando de nuevo otra vez

--- spacereview23.js
+++ spacereview23.js
@@ -1,24 +1,29 @@
-//Arrays
+// //Arrays
 
-//Create and array of numbers. Sum all of the numbers. Alert the sum.
-let arr = [3,5,5]
+// //Create and array of numbers. Sum all of the numbers. Alert the sum.
+// let arr = [3,5,5]
 
 
 
-alert(arr.reduce((acc,c) => acc + c, 0))
+// alert(arr.reduce((acc,c) => acc + c, 0))
 
-//Create a function that takes in an array of numbers
-//Return a new array of numbers that is every original number squared
+// //Create a function that takes in an array of numbers
+// //Return a new array of numbers that is every original number squared
 
-let newArr = arr => arr.map(num => num**2)
+// let newArr = arr => arr.map(num => num**2)
 
-//Create a function that takes string
-//Print the reverse of that string to the console
+// //Create a function that takes string
+// //Print the reverse of that string to the console
 
-let unoReverse = str => console.log(str.split('').reverse().join(''))
+// let unoReverse = str => console.log(str.split('').reverse().join(''))
 
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
-const palindromeCheck = str => alert( str === str.split('').reverse().join('') )
+//const palindromeCheck = str => alert( str === str.split('').reverse().join('') )
 
-palindromeCheck('racecar')
+//palindromeCheck('racecar')
+
+
+let filter_list = l => l.filter(str => typeof str != 'string')
+
+console.log(filter_list([1,2,'a','b']))
