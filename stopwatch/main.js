8ce5fef0c3ce024f533ffec29405e1a17baaea99
Federico Chaves <90200540+fedechaves@users.noreply.github.com>
2022-05-02 14:36:02 -0400

    Add files via upload
    HW youtube vid OOP

---
+++ stopwatch/main.js
@@ -0,0 +1,36 @@
+//stopwatch
+function StopWatch() {
+    let startTime, endTime, running, duration  = 0;
+    
+    this.start = function(){
+        if (running)
+            throw new Error('Stopwatch is already running');
+
+        running = true;
+
+        startTime = new Date();
+    }
+
+    this.stop = function(){
+        if (!running)
+            throw new Error ('stopwatch is not started');
+
+        running = false;
+        
+        endTime = new Date();
+
+        const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+        duration += seconds;
+    };
+    this.reset = function(){
+        starTime = null;
+        endTime = null;
+        running = false;
+        duration = 0;
+    };
+    
+    Object.defineProperty(this, 'duration', {
+        get: function() {return duration; }
+    });
+}
+
